2f6881c9a2fecb7de6461ee92cf4e83dadf2d06d
RafalSkwara <rafal.skwara@gmail.com>
2017-02-09 17:12:50 +0000

    works done on header

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(window).resize(function(){var a=$("ul.nav.navbar-nav"),l=$(window).width();l<=768?a.removeClass("pull-right"):a.hasClass("pull-right")||a.addClass("pull-right")})});
+$(document).ready(function(){$("nav ul li").on("click",function(){$("nav ul li").removeClass("active"),$(this).addClass("active")})});
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,8 @@
 $(document).ready(function(){
-	$(window).resize(function(){
-		var $navigation = $('ul.nav.navbar-nav');
-		var $windowWidth = $( window ).width();
-		if ($windowWidth <= 768) {
-			$navigation.removeClass('pull-right');
-
-		} else{
-			if(!$navigation.hasClass('pull-right')) {
-				$navigation.addClass('pull-right');
-			} //end inner if
-		} // end outer if
-	}); // end resize
+$("nav ul li").on("click", function(){
+	$("nav ul li").removeClass("active");
+	$(this).addClass("active");
+});
 
 	
 }); // end ready
