b4597098ca33d430782b592bafeea51e50882651
RafalSkwara <rafal.skwara@gmail.com>
2017-06-06 15:55:53 +0200

    Media queries step 1

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,28 @@ $(document).ready(function(){
 	}, 500); // scroll to the top of the page after load
 
 
+	// make sure we know id screen is big or small.
+	// It'll come in handy later
+	// Adds and removes body class depending on screen width.
+	function screenClass() {
+	    if($(window).innerWidth() > 960) {
+	        $('body').addClass('big-screen').removeClass('small-screen');
+	        $('.nav-list').css('display', 'flex');
+	    } else {
+	        $('body').addClass('small-screen').removeClass('big-screen');
+	        $('.nav-list').css('display', '');
+	    }
+	}
+
+	// Fire.
+	screenClass();
+
+	// And recheck when window gets resized.
+	$(window).bind('resize',function(){
+	    screenClass();
+	});
+
+
 
 	$("nav ul li").on("click", function(){
 		$("nav ul li").removeClass("active");
@@ -31,7 +53,6 @@ $(document).ready(function(){
 		// scrollspy logic
 		// for nav links
 		if(bodyScroll > $contentOffset || elScroll >= $contentOffset) {
-			console.log(elScroll);
 			$("nav ul li").removeClass("active");
 
 			if(elScroll > $offKontakt){
@@ -66,4 +87,18 @@ $(document).ready(function(){
 			} //end if
 			
 		}); //end click
+
+		//mobile menu logic
+		if ($('body').hasClass('small-screen')) {
+			$('#menu-toggle').click(function(){
+				$('.nav-list').toggle();
+				$('nav ul li').removeClass('active');
+				$('nav ul li').click(function(){
+					$('.nav-list').hide();
+				});
+			});	
+		} 
+		
+		
+		
 }); // end ready
